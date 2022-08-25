6f60a29bcf6222cb0d0bd9e5b127b66cf97a51b0
AW3 <>
2022-08-25 12:32:11 -0700

    fetch API

--- server/server.js
+++ server/server.js
@@ -3,10 +3,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+const helloRouter = require('./api/routes/hello');
 
 app.use(cors());
 app.use(express.json());
+app.use('/hello', helloRouter);
+
+
+
 app.listen(port, () => {
 console.log(`Server is running on port: ${port}`);
 });
